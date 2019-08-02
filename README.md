dcaa8ac5940630a5698cb500fc6cc67f90d5b127
Adam Prchal <PrchalAdam@outlook.cz>
2019-08-02 11:55:22 +0200

    Making UI simple + other improvements

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Layers-properties-export-script is a Photoshop script for exporting layers properties (position and size) and saving them into Nanits Composer's config file of comic book project.
 
 ## Installation
-Copy `[AP] Export to config.jsx` and `json2.js` to:`...Adobe\Adobe Photoshop [YOUR VERSION]\Presets\Scripts\` and restart Photoshop.
+
+Copy `[AP] Export to config.jsx` and `json2.js` and to:`...Adobe\Adobe Photoshop [YOUR VERSION]\Presets\Scripts\` and restart Photoshop.
 
 ## Requirements - IMPORTANT :fire:
 
@@ -15,21 +16,17 @@ Copy `[AP] Export to config.jsx` and `json2.js` to:`...Adobe\Adobe Photoshop [YO
 - Exported layers copied to folders
   - Parallax - `...\[COMICBOOK FOLDER]\par\img\`
   - Lettering - `...\[COMICBOOK FOLDER]\par\lang\cz|en|es\`
-- Back up your original `config.json` just for a case
 
-## How to use 
+## How to use
 
 1. Check if your PSB is arranged same way [CUTTING TEMPLATE](https://drive.google.com/file/d/1mNy_khdUNIUAA_FrlTGaM6qN0cuRLKDV/view) is
 2. File -> Scripts -> [AP] Export to config
-3. Write page/file number 
-4. Check "Last page" checkbox if it's last page/file (or only page)
-5. Using "Select config.json" find `config.json` in comic book folder you have created with Composer
-6. Click "Ok"
-7. Wait until "Export completed successfully" shows up
-8. On your desktop there should be new `config.json`, copy it to comic book folder and replace the old one
-9. Open Composer and check if it worked :100:
+3. Using "Select config.json" find `config.json` in comic book folder you have created with Composer.
+4. Click "Ok"
+5. Wait until "Export completed successfully" dialog shows up
+6. Check that there is a `config-backup.json` file (Backup of original `config.json`)
+7. Open Composer and check if it worked :100:.
 
 ## How it works
 
-After running this script and selecting `config.json` all layers properties are added to `config.json`. That means you don't have to place parallax and lettering layers into Nanits Composer manually. You only need to put layers exported as PNG images into `par\img` and `\par\lang\cz|en|es\  `folder of your project. 
-
+After running this script and selecting `config.json` all layers properties are added to `config.json`. That means you don't have to place parallax and lettering layers into Nanits Composer manually. You only need to put layers exported as PNG images into `par\img` and `\par\lang\cz|en|es\`folder of your project.
--- [AP] Export to config.jsx
+++ [AP] Export to config.jsx
@@ -1,33 +1,42 @@
+
 #target photoshop
-#include json2.js
+#include 'json2.js'
+
+
+var usedID = [];
+var letteringGroup = app.activeDocument.layerSets.getByName('Lettering').layerSets;
+
 
 /* Object for making operations with layer groups easier */
 var groups = {
     'lang': {
-        'en': app.activeDocument.layerSets.getByName('Lettering').layerSets.getByName('ENG').layers,
-        'cz': app.activeDocument.layerSets.getByName('Lettering').layerSets.getByName('CZE').layers,
+        //'en': app.activeDocument.layerSets.getByName('Lettering').layerSets.getByName('ENG').layers,
+        //'cz': app.activeDocument.layerSets.getByName('Lettering').layerSets.getByName('CZE').layers,
         //'sfx': app.activeDocument.layerSets[0].layerSets.getByName('SFX').layers
     },
     'img': app.activeDocument.layerSets.getByName('Parallax').layers
 }
 
 function main() {
-
     try {
-
+        detectLanguages();
         var inputData = inputWindow();  // UI interface
         inputData;
-
         const loadedJSON = inputData[0] // Loaded and parsed config.json
         const page = inputData[1];      // Page/PSB number 
         const lastPage = inputData[2];  // Bool if active page/PSB is the last one
+        const folderPath = inputData[3].substring(0, inputData[3].length - 12);
+        //const topOffset = getTopOffset(loadedJSON, page, lastPage); // Gets top offset for layers in case of having multiple PSB files
 
-        const topOffset = getTopOffset(loadedJSON, page, lastPage); // Gets top offset for layers in case of having multiple PSB files
+        var fileBackup = new File(Folder.desktop + "/config-backup.json");   // Creates .json file on Desktop
+        fileBackup.open("w", "TEXT", "????");                                // Opens file for editing
+        fileBackup.writeln(JSON.stringify(loadedJSON, null, "\t"));          // Stringify and formats object data into file
+        fileBackup.close();
 
         importImgLayers(loadedJSON);    // Insert each parallax layer properties into object
         importLangLayers(loadedJSON);   // Insert each lettering layer properties into object
 
-        var file = new File(Folder.desktop + "/config.json");   // Creates .json file on Desktop
+        var file = new File(folderPath + "/config.json");       // Creates .json file on Desktop
         file.open("w", "TEXT", "????");                         // Opens file for editing
         file.writeln(JSON.stringify(loadedJSON, null, "\t"));   // Stringify and formats object data into file
         file.close();                                           // Closes file
@@ -41,15 +50,18 @@ function main() {
                 var activeLang = langKeys[i];
                 for (var y = 0; y <= (groups.lang[activeLang].length - 1); y++) {
                     var currentLayer = groups.lang[activeLang][y];
+                    var fileID = generateRandomString();
+                    var aFile = File(folderPath + "\\par\\lang\\" + activeLang + '\\' + currentLayer.name + ".png");
+                    aFile.rename(fileID + ".png");
                     var currentLayerObject = {
-                        "src": ".\\par\\lang\\" + activeLang + '\\' + currentLayer.name,
+                        "src": ".\\par\\lang\\" + activeLang + '\\' + fileID,
                         "size": {
                             "x": getLayerWidth(currentLayer),
                             "y": getLayerHeight(currentLayer)
                         },
                         "position": {
                             "x": currentLayer.bounds[0].as("px"),
-                            "y": currentLayer.bounds[1].as("px") + topOffset
+                            "y": currentLayer.bounds[1].as("px") //+ topOffset
                         },
                         "offset": {
                             "top": activeJSON.metadata.projectWidth,
@@ -67,15 +79,18 @@ function main() {
         function importImgLayers(activeJSON) {
             for (var i = 0; i <= (groups.img.length - 1); i++) {
                 var currentLayer = groups.img[i];
+                var fileID = generateRandomString();
+                var aFile = File(folderPath + "\\par\\img\\" + currentLayer.name + ".png");
+                aFile.rename(fileID + ".png");
                 var currentLayerObject = {
-                    "src": ".\\par\\img\\" + currentLayer.name,
+                    "src": ".\\par\\img\\" + fileID,
                     "size": {
                         "x": getLayerWidth(currentLayer),
                         "y": getLayerHeight(currentLayer)
                     },
                     "position": {
                         "x": currentLayer.bounds[0].as("px"),
-                        "y": currentLayer.bounds[1].as("px") + topOffset
+                        "y": currentLayer.bounds[1].as("px") //+ topOffset
                     },
                     "offset": {
                         "top": activeJSON.metadata.projectWidth,
@@ -91,9 +106,9 @@ function main() {
 
         function inputWindow() {
             var myWindow = new Window("dialog", "Form");
-            myWindow.add("statictext", undefined, "Page number:");
-            var pageNumber = myWindow.add("edittext");
-            var lastPage = myWindow.add("checkbox", undefined, "Last page");
+            /*myWindow.add("statictext", undefined, "Page number:");
+             var pageNumber = myWindow.add("edittext");
+             var lastPage = myWindow.add("checkbox", undefined, "Last page"); */
             myWindow.add("statictext", undefined, "Path to config.json:");
             var pathToJSON = myWindow.add("edittext");
             var findFile = myWindow.add("button", undefined, "Select config.json")
@@ -109,11 +124,36 @@ function main() {
             }
             if (myWindow.show() == 1) {
                 var loadedJSON = loadJSON(pathToJSON.text);
-                return [loadedJSON, pageNumber.text, lastPage.value];
+                return [loadedJSON, 0, 0, pathToJSON.text];
             } else {
                 return;
             }
             myWindow.show();
+        }
+
+        function generateRandomString() {
+            const length = 20;
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for (var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            var n = false;
+
+            for (var i = 0; i < usedID.length; i++) {
+                if (text == usedID[i]) n = true;
+            }
+
+            if (n == false) {
+
+                usedID.push(text);
+                return text;
+            }
+            else {
+                generateRandomString();
+            }
+
         }
 
         function loadJSON(pathToJSON) {
@@ -159,6 +199,17 @@ function main() {
                 }
             }
             return arrayWithKeys;
+        }
+
+        function detectLanguages() {
+            for (var i = 0; i < letteringGroup.length; i++) {
+                var langName = letteringGroup[i].name;
+                alert(langName);
+                if (langName == "ENG") groups.lang.en = letteringGroup.getByName(langName).layers;
+                if (langName == "CZE") groups.lang.cz = letteringGroup.getByName(langName).layers;
+                if (langName == "GER") groups.lang.de = letteringGroup.getByName(langName).layers;
+                if (langName == "ESP") groups.lang.es = letteringGroup.getByName(langName).layers;
+            }
         }
 
         function getLayerWidth(layer) {
